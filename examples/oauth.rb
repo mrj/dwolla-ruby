99bbc01e2b8bf24f2bbd8808d739363fc3a47a7a
David Stancu <dstancu@nyu.edu>
2014-09-22 17:32:22 -0400

    added oauth-refresh oauth example

--- examples/oauth.rb
+++ examples/oauth.rb
@@ -18,17 +18,34 @@ redirect_uri = 'http://localhost:4567/oauth_return'
 #   Create an authentication URL
 #   that the user will be redirected to
 get '/' do
-    authUrl = Dwolla::OAuth.get_auth_url(redirect_uri)
-    "To begin the OAuth process, send the user off to <a href=\"#{authUrl}\">#{authUrl}</a>"
+  authUrl = Dwolla::OAuth.get_auth_url(redirect_uri)
+  "To begin the OAuth process, send the user off to <a href=\"#{authUrl}\">#{authUrl}</a>"
 end
 
 
 # STEP 2:
 #   Exchange the temporary code given
 #   to us in the querystring, for
-#   a never-expiring OAuth access token
+#   an expiring OAuth access token and refresh token pair.
 get '/oauth_return' do
-    code = params['code']
-    token = Dwolla::OAuth.get_token(code, redirect_uri)
-    "Your never-expiring OAuth access token is: <b>#{token}</b>"
+  code = params['code']
+  info = Dwolla::OAuth.get_token(code, redirect_uri)
+  token = info['access_token']
+  refresh_token = info['refresh_token']
+  "Your expiring OAuth access token is: <b>#{token}</b>, and your refresh token is <b>#{refresh_token}</b>"
+end
+
+# STEP 3:
+#
+#   The array returned in step 2 as 'info' also contains
+#   expiration times for when the OAuth token will become
+#   invalid. Use this method to refresh your token with
+#   the provided refresh token.
+#
+get '/oauth_refresh' do
+  refresh_token = params['refresh_token']
+  info = Dwolla::OAuth.refresh_auth(refresh_token)
+  token = info['access_token']
+  refresh_token = info['refresh_token']
+  "Your expiring OAuth access token is: <b>#{token}</b>, and your refresh token is <b>#{refresh_token}</b>"
 end
